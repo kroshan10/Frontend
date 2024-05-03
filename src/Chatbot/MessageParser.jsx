a341a3f0bf282ad991cf0f2a6116d018fbd900f6
Prahladkumar11 <Prahladkuma11@gmail.com>
2024-05-03 14:15:44 +0530

    no change

--- src/Chatbot/MessageParser.jsx
+++ src/Chatbot/MessageParser.jsx
@@ -1,16 +1,41 @@
 import React from 'react';
+import { createChatBotMessage } from'react-chatbot-kit';
 
-const MessageParser = ({ children, actions }) => {
+
+const MessageParser = ({ children, actions, }) => {
   const { checker } = children.props.state;
-  let name;
   const parse = (message) => {
 
-
+    
     if(checker === "name") {
-      name=message;
-      actions.afterNameMessage(name);
+      children.props.state.name=message
+      actions.afterNameMessage(message);
     }
-
+    else if(checker === "heartDisease") {
+      const ques = {
+        "What is your age?": '',
+        "What is your sex? (1 for male, 0 for female)": '',
+        "What type of chest pain are you experiencing?": '',
+        "What is your resting blood pressure?": '',
+        "What is your serum cholesterol level?": '',
+        "Do you have fasting blood sugar > 120 mg/dl? (1 for true, 0 for false)": '',
+        "What are your resting electrocardiographic results?": '',
+        "What is your maximum heart rate achieved?": '',
+        "Do you experience exercise-induced angina? (1 for yes, 0 for no)": '',
+        "What is your ST depression induced by exercise relative to rest?": '',
+        "What is the slope of the peak exercise ST segment?": '',
+        "How many major vessels are colored by fluoroscopy?": '',
+        "What is your thalassemia type?": ''
+      };
+      
+      for (const question in ques) {
+        createChatBotMessage(question);
+        const answer = message;
+        ques[question] = answer;
+        console.log(ques,name);
+      }
+      
+    }
   }
 
   return (
